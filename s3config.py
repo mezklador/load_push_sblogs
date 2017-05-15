10d86d66143c6bc0be7181a2a62797eb51ce651d
stevostean <se@studiomzk.com>
2017-05-16 01:27:28 +0200

    remove s3 config for git

--- s3config.py
+++
@@ -1,6 +0,0 @@
-AWS_S3_KEY = 'AKIAJECWOQSLZYPDBCOQ'
-AWS_S3_SECRET = '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'
-
-if __name__ == '__main__':
-    print("Key: {}".format(AWS_S3_KEY))
-    print("Secret: {}".format(AWS_S3_SECRET))
