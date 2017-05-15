94dff275ff0ec2d83e628dbcf465f5c963b2bfcd
stevostean <se@studiomzk.com>
2017-05-16 01:10:26 +0200

    I think it's almost done before deployment?

---
+++ aws.py
@@ -0,0 +1,58 @@
+from s3config import AWS_S3_KEY, AWS_S3_SECRET
+from datetime import datetime
+import boto3
+import botocore
+
+import logging
+import logging.config
+import os
+import sys
+from time import time
+
+
+logging.config.fileConfig('log_config.ini',defaults={'logfilename':'apilogs/uploads/state_of_union.log'})
+logger = logging.getLogger(__name__)
+
+# Session is made for custom session towards some region?
+# FROM: http://stackoverflow.com/questions/33577503/how-to-configure-authorization-mechanism-inline-with-boto3#answer-33668804
+session = boto3.session.Session(region_name="eu-central-1")
+
+client = boto3.client('s3',
+                       aws_access_key_id=AWS_S3_KEY,
+                       aws_secret_access_key=AWS_S3_SECRET,
+                       config=boto3.session.Config(signature_version="s3v4"))
+s3 = boto3.resource("s3")
+
+
+if __name__ == "__main__":
+    '''
+    buckets = [bucket['Name'] for bucket in client.list_buckets()['Buckets']]
+    print(", ".join(buckets))
+
+    for bucket in s3.buckets.all():
+        print(bucket.name, sep=", ")
+
+    for k in client.list_objects(Bucket='elz')['Contents']:
+        print(k['Key'])
+    '''
+    
+    start = time()
+
+    # list all files in logs directory
+    logfiles = os.listdir('logs')
+
+    for logfile in logfiles:
+        file_to_upload = os.path.join(os.getcwd(), 'logs', logfile) 
+        filename = file_to_upload.split('/')[-1]
+        #print(file_to_upload, filename)
+        
+        try:
+            #FROM: http://stackoverflow.com/questions/42008288/upload-file-to-amazon-cloud-subfolder-using-python-boto3#answer-42008433
+            client.upload_file(file_to_upload, "elz", f"sb_logs/{filename}")
+            os.unlink(file_to_upload)
+            end = time() - start
+            logger.info(f"Uploaded {filename} to AWS S3 for {end:.4f} sec.")
+
+        except botocore.exceptions.ClientError as e:
+            logger.warning(f"Problem with S3: {e}")
+            sys.exit(1)
--- dwn_sb_logs.py
+++ dwn_sb_logs.py
@@ -1,27 +1,62 @@
 #!/home/me/Documents/Codes/python/sb_dwn_logs/env/bin/python3
 
+from datetime import datetime
+import logging
+import logging.config
 import os
+import sys
+from time import time
 
 import requests
 from tqdm import tqdm
 
 
+logging.config.fileConfig('log_config.ini', defaults={'logfilename':'apilogs/downloads/timeline.log'})
+logger = logging.getLogger(__name__)
+'''
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s ~ %(levelname)s: %(message)s")
+fh = logging.FileHandler("apilogs/downloads/timeline.log")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+'''
+
 sb_logs_file = "http://soda-bar.fr/hide/loading_page.log"
 
 if __name__ == '__main__':
+    start = time()
     directory_name = "logs"
     here = os.getcwd()
     logs_dir = os.path.join(here, directory_name)
 
     if not os.path.isdir(logs_dir):
         os.makedirs(logs_dir)
+    
+    try:
+        r = requests.get(sb_logs_file, stream=True)
+        total_size = int(r.headers.get('content-length', 0))
 
-    r = requests.get(sb_logs_file, stream=True)
-    total_size = int(r.headers.get('content-length', 0))
+        now = datetime.now()
 
-    with open(os.path.join(logs_dir, 'current_sb_log.log'), 'wb') as f:
-        for data in tqdm(r.iter_content(32*1024),
-                         total=total_size, unit='B',
-                         unit_scale=True):
-            f.write(data)
+        filename_date = f"{now.year}{now.month}{now.day}-{now.hour}_{now.minute}_{now.second}.log"
+
+        with open(os.path.join(logs_dir, filename_date), 'wb') as f:
+            for data in tqdm(r.iter_content(32*1024),
+                             total=total_size, unit='B',
+                             unit_scale=True):
+                f.write(data)
+        end = time() - start
+        logger.info(f"Downloaded {filename_date} to /logs/ in {end:.4f} sec.")
+
+    except requests.exceptions.RequestException as e:
+        logger.warning(f"Bad request: {e}")
+        sys.exit(1)
+
+    except requests.exceptions.Timeout as tm:
+        logger.warning(f"Request Timeout: {tm}")
+        sys.exit(1)
+
+    except requests.exceptions.HTTPError as he:
+        logger.warning(f"HTTP error: {he}")
+        sys.exit(1)
 
